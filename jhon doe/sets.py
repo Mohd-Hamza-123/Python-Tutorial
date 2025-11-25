591ea677a1c40d226827eceea46b3267c6471bca
Mohd Hamza <134189864+Mohd-Hamza-123@users.noreply.github.com>
2025-11-25 19:58:43 +0530

    Add files via upload

---
+++ jhon doe/sets.py
@@ -0,0 +1,27 @@
+roll_no = {1,2,3,4,1}
+print(roll_no)
+
+# methods 
+
+# add()
+
+roll_no.add(5)
+roll_no.add("5")
+roll_no.add(5)
+print(roll_no)
+
+
+# clear 
+
+my_set = {1, 2, 3, 4 , True}
+
+# my_set.clear()
+# my_set.remove(5)
+# my_set.discard(5)
+my_set.pop()
+print(my_set)
+
+
+my_set = {"Hello","hi","bye" , "UUUU"}
+my_set.pop()
+print(my_set)
