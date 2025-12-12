afdcc5b8943487a0a748cc2aaf8aa07ca60f625c
Mohd Hamza <134189864+Mohd-Hamza-123@users.noreply.github.com>
2025-12-12 19:53:42 +0530

    Add files via upload

---
+++ jhon doe/basic-functions.py
@@ -0,0 +1,26 @@
+
+def printFixValues():
+    a = 44
+    b = 98 
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a / b)
+
+# printFixValues()
+print("---------------------------")
+# printFixValues()
+
+def printValues(a,b):
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a//b)
+
+
+result = printValues(1,3) # calling function printValues
+# printValues(6,8)
+# print('result : ',result)
+
+
+
