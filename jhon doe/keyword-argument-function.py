5da3f001484c311ac14016001a6c0cb1cd38c4d8
Mohd Hamza <134189864+Mohd-Hamza-123@users.noreply.github.com>
2025-12-13 20:01:44 +0530

    Add files via upload

---
+++ jhon doe/keyword-argument-function.py
@@ -0,0 +1,10 @@
+def register(username,email,password):
+    print(username)
+    print(email)
+    print(password)
+
+register(password="fkd#$23", email= "hamza@gmail.com" ,username = "Mohd Hamza")
+
+# list = [23,54,6,4,24,4]
+# list.sort(reverse=True)
+# print(list)
---
+++ jhon doe/unknown-argument-function.py
@@ -0,0 +1,15 @@
+def total(*args):
+    result = 0
+    for num in  args:
+        result = result + num 
+    return result
+
+result = total(4,43,564,2,45,56)
+print(result)
+
+import dictFunc
+
+dictFunc.greet("Hamza")
+
+
+
