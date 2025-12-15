b9b9dfb4ff76a0400eec61ac1e6e31d02265eef9
Mohd Hamza <134189864+Mohd-Hamza-123@users.noreply.github.com>
2025-12-15 20:05:17 +0530

    Add files via upload

---
+++ zainab/conditionals-basic.py
@@ -0,0 +1,34 @@
+# age = 18
+
+# if age >= 18:
+#     print("You are an adult")
+#     print(f"age is {age}")
+# else:
+#     print("Your age is less than 19")
+
+# print("HEllo world")
+
+
+
+
+number = 2
+
+if number >= 7:
+    print("number is positive")
+    print(f"number is {number}")
+else:
+    print("number is negative")
+
+age = 15
+
+if age >= 18:
+    print("age is greater than 18")
+else:
+    print("age is less than 18")
+
+string = "Python"
+
+if string == "python":
+    print("string is equal to python")
+else:
+    print("string is not python")
---
+++ zainab/conditionals-elif.py
@@ -0,0 +1,9 @@
+
+username = input("Enter username : ")
+
+if len(username) < 3:
+    print("length is rejected")
+elif length :
+    print("length is rejected")
+else:
+    print("length is accepted")
