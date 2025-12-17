d15b23e68355f0c1da8d852aa396136bc58faf53
Mohd Hamza <134189864+Mohd-Hamza-123@users.noreply.github.com>
2025-12-17 20:00:04 +0530

    Add files via upload

---
+++ zainab/conditions-questions.py
@@ -0,0 +1,44 @@
+# age = int(input("Enter your age : "))
+
+# if age >= 18:
+#     print("Adult")
+# else:
+#     print("Minor")
+
+# x = int(input("Enter value of x : "))
+# y = int(input("Enter value of y : "))
+# if x > y:
+#     print(f"x is greater than y. x = {x}      y = {y}")
+# elif x == y:
+#     print("x is equal to y")
+# else:
+#     print("x is less than y")
+
+# a = []
+
+
+
+# if len(a) == 0:
+#     print("list is empty")
+# else:
+#     print("list is not empty")
+
+
+# string = input("Enter your word : ")
+# if string.count("py") >= 1 :
+#     print("string contain py")
+# else:
+#     print("string doesn't contain py")
+
+# num = int(input("Enter a number : "))
+
+# if num % 2 == 0:
+#     print(f"Number {num} = even")
+# else:
+#     print(f"Number {num} = odd")
+
+character = input("Enter your character : ")
+if character == "a" or character == "e" or character == "i" or character == "o" or character == "u":
+    print(f"Character is vowel : {character}")
+else:
+    print(f"Character is consonent : {character}")
